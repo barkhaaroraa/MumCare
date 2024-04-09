4267ec4c933b82c85aa8d55719d6bf0278f2b876
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2024-04-10 00:20:38 +0530

    Update YogaNutritionPage.jsx

--- src/pages/YogaNutritionPage.jsx
+++ src/pages/YogaNutritionPage.jsx
@@ -7,7 +7,7 @@ return (
  <div>
   <NavBar/>
       <header className="py-4 text-center">
-      <h1 className="text-5xl font-semibold text-pink-700 pt-8">Yoga and Nutrition Tips for Women</h1>
+      <h1 className="text-pink-800 md:text-6xl sm:text-4xl text-4xl font-bold md:py-8">Yoga and Nutrition After Pregnancy!</h1>
       </header>
     <YogaCarousel/>
  </div>
