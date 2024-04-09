7437e8e25441f38c9ca894d74e61d9730cad3067
Prithwi Hegde <115262737+Prithwi32@users.noreply.github.com>
2024-04-09 23:23:18 +0530

    Update YogaNutritionPage.jsx

--- src/pages/YogaNutritionPage.jsx
+++ src/pages/YogaNutritionPage.jsx
@@ -1,9 +1,11 @@
 import YogaCarousel from "../components/YogaCarousel";
+import NavBar from "../components/NavBar"
 
 function YogaNutritionPage() {
 
 return (
- <div className="container mx-auto px-4">
+ <div>
+  <NavBar/>
       <header className="py-4 text-center">
       <h1 className="text-5xl font-semibold text-pink-700 pt-8">Yoga and Nutrition Tips for Women</h1>
       </header>
