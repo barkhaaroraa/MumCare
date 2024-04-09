965b1579c3f87d6f2f5b2a87794cb7026d56ec9a
Prithwi <prithwi.online11@gmail.com>
2024-04-09 23:01:17 +0530

    styling navbar and fixes alignment issues in it

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logo from '../assets/images/logo.png';
+import "../index.css"
 
 const NavBar = () => {
+  const {pathname} = useLocation()
   return (
-    <nav className="flex items-center justify-between bg-pink-800 py-3 px-4" style={{ fontFamily: 'Montserrat, sans-serif' }}>
+    <nav className="flex items-center justify-between bg-gradient-to-br from-pink-600 via-pink-400 to-pink-800 py-2 px-4" style={{ fontFamily: 'Poppins, Montserrat, sans-serif' }}>
       <div className="flex items-center">
-        <img src={logo} alt="Logo" className="h-12 w-auto mr-4 ml-2" />
+        <img src={logo} alt="Logo" className="h-16 w-auto mr-4 ml-2" />
         <span style={{
           fontWeight: 'bold',
-          fontSize: '32px', 
-          color: 'rgb(211, 211, 185)',
+          fontSize: '32px',
+          color: '#FFFFDE',
           lineHeight: '50px',
-          paddingLeft: '30px'
+          paddingLeft: '30px',
+          textDecoration: 'none'
         }}>
           MumCares
         </span>
       </div>
+      <div className="flex items-center">
       <ul className="flex space-x-4 text-pink-300">
         <li className="ml-auto">
-          <Link to="/" className="text-lg italic" style={{ color: 'rgb(211, 211, 185)', fontSize: '22px' }}>Home</Link> {/* Adjusted font size */}
+        <NavLink to="/" className={`text-lg ${pathname === '/' ? 'active-link' : 'hover:border-b-2'}`} style={{ color: '#FFFFDE', fontSize: '22px'}}>Home</NavLink> 
         </li>
         <li>
-          <Link to="/relax" className="text-lg italic" style={{ color: 'rgb(211, 211, 185)', fontSize: '22px' }}>Relaxation</Link> {/* Adjusted font size */}
+        <NavLink to="/relax" className={`text-lg ${pathname === '/relax' ? 'active-link' : 'hover:border-b-2'}`} style={{ color: '#FFFFDE', fontSize: '22px' }}>Relaxation</NavLink> 
         </li>
         <li>
-          <Link to="/postpartum-recovery" className="text-lg italic" style={{ color: 'rgb(211, 211, 185)', fontSize: '22px' }}>Postpartum</Link> {/* Adjusted font size */}
+        <NavLink to="/postpartum-recovery" className={`text-lg ${pathname === '/postpartum-recovery' ? 'active-link' : 'hover:border-b-2'}`} style={{ color: '#FFFFDE', fontSize: '22px' }}>Postpartum</NavLink> 
         </li>
         <li>
-          <Link to="/community" className="text-lg italic" style={{ color: 'rgb(211, 211, 185)', fontSize: '22px' }}>Community</Link> {/* Adjusted font size */}
+        <NavLink to="/community" className={`text-lg ${pathname === '/community' ? 'active-link' : 'hover:border-b-2'}`} style={{ color: '#FFFFDE', fontSize: '22px' }}>Community</NavLink> 
         </li>
         <li>
-          <Link to="/depression" className="text-lg italic" style={{ color: 'rgb(211, 211, 185)', fontSize: '22px' }}>Depression</Link> {/* Adjusted font size */}
+        <NavLink to="/depression" className={`text-lg ${pathname === '/depression' ? 'active-link' : 'hover:border-b-2'}`} style={{ color: '#FFFFDE', fontSize: '22px' }}>Depression</NavLink> 
         </li>
       </ul>
-      <button className="bg-pink-200 text-pink-500 px-4 py-2 rounded-md font-normal text-lg ml-2">
+      <button className="bg-pink-200 text-pink-500 hover:text-pink-200 hover:bg-[#e45a88] px-4 py-3 rounded-md font-semibold text-xl ml-6">
         ANALYSE
       </button>
+      </div>
     </nav>
   );
 }
