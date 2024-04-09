24007fce4173f37e938842f3e5bde9208b22252c
Prithwi <prithwi.online11@gmail.com>
2024-04-09 19:09:10 +0530

    Added yoga tips part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import RelaxationPage from "./pages/RelaxationPage"
+import YogaNutritionPage from "./pages/YogaNutritionPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/relax" element={<RelaxationPage />} />
+          <Route path="/yoga" element={<YogaNutritionPage />} />
         </Routes>
       </Router>
     </>
